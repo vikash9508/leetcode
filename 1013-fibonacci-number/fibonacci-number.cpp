677aa419c8fbba4956142058971722b4bb6f8ddb
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-02-20 12:52:59 +0530

    Time: 3 ms (64.99%) | Memory: 8.4 MB (10.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
-    int fib(int n) {
-        //basic recursive method because here in question n=30
+    int fibo(int n,vector<int>&dp){
+        //base case
         if(n<=1)
         return n;
+        //if already calculated
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+     return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    }
+    int fib(int n) {
+        // //basic recursive method because here in question n=30
+        // if(n<=1)
+        // return n;
         
-        return fib(n-1)+fib(n-2);
+        // return fib(n-1)+fib(n-2);
+
+
+        //if constraint aur test case change so to avoid tle we use tod down memoization
+        vector<int>dp(n+1,-1);
+         return fibo(n,dp);
     }
 };
