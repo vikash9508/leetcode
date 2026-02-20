27e96164b7ea7d6b67bb72cbdec9820c22cda2f1
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-02-20 18:15:12 +0530

    Time: 3 ms (64.99%) | Memory: 8.1 MB (18.75%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int fibo(int n,vector<int>&dp){
-        //base case
-        if(n<=1)
-        return n;
-        //if already calculated
-        if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-     return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-    }
+    // int fibo(int n,vector<int>&dp){
+    //     //base case
+    //     if(n<=1)
+    //     return n;
+    //     //if already calculated
+    //     if(dp[n]!=-1)
+    //     {
+    //         return dp[n];
+    //     }
+    //  return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    // }
     int fib(int n) {
         // //basic recursive method because here in question n=30
         // if(n<=1)
@@ -20,7 +20,18 @@ public:
 
 
         //if constraint aur test case change so to avoid tle we use tod down memoization
-        vector<int>dp(n+1,-1);
-         return fibo(n,dp);
+        // vector<int>dp(n+1,-1);
+        //  return fibo(n,dp);
+         //bottom up approach
+         if(n<=1)
+         return n;
+         vector<int>dp(n+1,-1);
+         dp[0]=0;
+         dp[1]=1;//cant acess it when size 1 so it give error so to handle to uppar if statement
+         for(int i=2;i<=n;i++)
+         {
+            dp[i]=dp[i-1]+dp[i-2];
+         }
+         return dp[n];
     }
 };
