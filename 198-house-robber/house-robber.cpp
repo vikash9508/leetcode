a5c49b375c236b3588d8b993979fa94232e3a57b
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-02-22 16:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.13%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,6 +13,8 @@ public:
 //     return dp[index]=max(nums[index]+robber(index-2,nums,dp),robber(index-1,nums,dp));
 // }
     int rob(vector<int>& nums) {
+
+        //bottom up approach
         int n=nums.size();
         if(n==1)
         return nums[0];
@@ -20,14 +22,23 @@ public:
         if(n==2)
         return max(nums[0],nums[1]);
 
-        vector<int>dp(n,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        // vector<int>dp(n,-1);
+        // dp[0]=nums[0];
+        // dp[1]=max(nums[0],nums[1]);
+        // for(int i=2;i<n;i++)
+        // {
+        //     dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        // }
+        //  return dp[n-1];
+
+        int curr,prev=max(nums[0],nums[1]), prev2=nums[0];
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            curr=max(nums[i]+prev2,prev);
+            prev2=prev;
+            prev=curr;
         }
-         return dp[n-1];
+        return curr; //space optimazation
 
     } 
 };
