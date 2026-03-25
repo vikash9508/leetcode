10bac59486b86454a546c2ebb43a94b821efbe7a
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-03-25 15:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (61.59%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-int winner(vector<bool>&person,int n,int index,int person_left,int k)
-{
-    if(person_left==1)
-    {
-        for(int i=0;i<n;i++)
-        {
-            if(person[i]==0)
-            return i+1;
-        }
-    }
-    //find the position kill
-    int kill=(k-1)%person_left;
-    while(kill--)
-    {
-        index=(index+1)%n;
-        while(person[index]==1)//skip the eliminiated person
-        {
-            index=(index+1)%n;
-        }
-    }
-        person[index]=1;
-        //next alive person
-        while(person[index]==1)
-        {
-            index=(index+1)%n;
-        }
+// int winner(vector<bool>&person,int n,int index,int person_left,int k)
+// {
+//     if(person_left==1)
+//     {
+//         for(int i=0;i<n;i++)
+//         {
+//             if(person[i]==0)
+//             return i+1;
+//         }
+//     }
+//     //find the position kill
+//     int kill=(k-1)%person_left;
+//     while(kill--)
+//     {
+//         index=(index+1)%n;
+//         while(person[index]==1)//skip the eliminiated person
+//         {
+//             index=(index+1)%n;
+//         }
+//     }
+//         person[index]=1;
+//         //next alive person
+//         while(person[index]==1)
+//         {
+//             index=(index+1)%n;
+//         }
        
     
-     return winner(person,n,index,person_left-1,k);
+//      return winner(person,n,index,person_left-1,k);
+// }
+int winner(int n,int k)
+{
+    if(n==1)
+    return 0;
+
+    return (winner(n-1,k)+k)%n;
 }
     int findTheWinner(int n, int k) {
-        vector<bool>person(n,0);
-        return winner(person,n,0,n,k);
+        // vector<bool>person(n,0);
+        // return winner(person,n,0,n,k);
+        return winner( n, k)+1;
     }
 };
