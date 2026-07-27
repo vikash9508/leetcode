372a03d3ea387db9b59b410ba92e68ea4847fc0c
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-07-27 23:58:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (8.86%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>result;
+       for(int i=0;i<=n;i++) 
+       {
+        result.push_back(__builtin_popcount(i));
+       }
+       return result;
+    }
+};
