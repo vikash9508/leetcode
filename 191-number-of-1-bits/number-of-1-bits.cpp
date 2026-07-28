f70e53a2dc08555c2240769b795b326f56bd4602
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-07-28 17:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.49%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count=0;
-        for(int i=31;i>=0;i--)
-        {
-            if(((n>>i)&1)==1)
-                count++;
-        }
-        return count;
+        return __builtin_popcount(n);
     }
 };
