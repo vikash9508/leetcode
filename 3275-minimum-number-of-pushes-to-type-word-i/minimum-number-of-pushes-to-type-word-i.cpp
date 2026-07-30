0c95f6651a21a3bf9538b140b287972a1161eb3c
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-07-30 11:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (19.48%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+         int result=0;
+        unordered_map<int,int>mp;
+        int assign_key=2;
+        for(char &ch:word)
+        {
+            if(assign_key>9)
+            assign_key=2;
+
+            mp[assign_key]++;
+            result+=mp[assign_key];
+
+            assign_key++;
+        }
+        return result;  
+    }
+};
