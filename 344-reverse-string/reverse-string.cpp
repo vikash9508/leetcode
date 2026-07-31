8663292d053b6a33cea39ae384db25e0378a829b
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-07-31 16:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (53.62%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+void solve(vector<char>&s,int left,int right)
+{
+    if(left>right)
+    {
+        return;
+    }
+    swap(s[left],s[right]);
+    solve(s,left+1,right-1);
+}
     void reverseString(vector<char>& s) {
-        int i=0;
-        int j=s.size()-1;
-        while(i<j)
-        {
-            swap(s[i],s[j]);
-            i++;
-            j--;
-        }
+        solve(s,0,s.size()-1);
     }
 };
