05b2b42eb33d18160eb7b1d002e267b4878ff2f6
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-07-31 16:07:48 +0530

    Time: 5 ms (1.94%) | Memory: 28.4 MB (5.23%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-void solve(vector<char>&s,int left,int right)
-{
-    if(left>right)
-    {
-        return;
-    }
-    swap(s[left],s[right]);
-    solve(s,left+1,right-1);
-}
     void reverseString(vector<char>& s) {
-        solve(s,0,s.size()-1);
+        deque<char>dq;
+        for(char ch:s)
+        {
+            dq.push_front(ch);
+        }
+        for(int i=0;i<s.size();i++)
+        {
+            s[i]=dq[i];
+        }
     }
 };
