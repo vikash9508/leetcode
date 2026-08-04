435a76ff8fcf2f9e40c0f55a2bf36568cd9efb09
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-08-04 16:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (5.52%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
-
-bool areEqual(stack<char> s1, stack<char> s2)
+bool isequal(stack<char>s1,stack<char>s2)
 {
-    if(s1.size() != s2.size())
-        return false;
-
-    while(!s1.empty())
+    if(s1.size()!=s2.size())
+    return 0;
+    while(!s1.empty() && !s2.empty())
     {
-        if(s1.top() != s2.top())
-            return false;
-
+    if(s1.top()==s2.top())
+    {
         s1.pop();
         s2.pop();
     }
-    return true;
-}
-
-bool backspaceCompare(string s, string t) {
-
-    stack<char> st1, st2;
-
-    for(int i=0;i<s.size();i++)
+    else
     {
+        return 0;
+    }
+    }
+    return 1;
+}
+    bool backspaceCompare(string s, string t) {
+       stack<char>s1;
+       stack<char>s2;
+       for(int i=0;i<s.size();i++)
+       {
         if(s[i]=='#')
         {
-            if(!st1.empty())
-                st1.pop();
+            if(!s1.empty())
+            s1.pop();
+
         }
         else
-            st1.push(s[i]);
-    }
-
-    for(int i=0;i<t.size();i++)
-    {
+        {
+            s1.push(s[i]);
+        }
+       } 
+        for(int i=0;i<t.size();i++)
+       {
         if(t[i]=='#')
         {
-            if(!st2.empty())
-                st2.pop();
+            if(!s2.empty())
+            s2.pop();
+
         }
         else
-            st2.push(t[i]);   
-    }
+        {
+            s2.push(t[i]);
+        }
+       } 
 
-    return areEqual(st1,st2);
-}
+       return isequal(s1,s2);
+    }
 };
