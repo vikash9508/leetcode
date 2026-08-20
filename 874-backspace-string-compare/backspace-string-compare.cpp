de250b5a39d67c4b042539fde5786a040d5d272f
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-08-20 11:01:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (86.35%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    bool isequal(stack<char>s1,stack<char>s2)
-    {
-        if(s1.size()!=s2.size())
-        {
-            return 0;
-        }
-        while(!s1.empty() && !s2.empty())
-        {
-            if(s1.top()==s2.top())
-            {
-                s1.pop();
-                s2.pop();
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        return 1;
-        
-    }
     bool backspaceCompare(string s, string t) {
-        stack<char>st1;
-        stack<char>st2;
-        for(auto ch:s)
+        string a,b;
+        for(char ch:s)
         {
             if(ch=='#')
             {
-                if(!st1.empty())
-                st1.pop();
+                if(!a.empty())
+                {
+                    a.pop_back();
+                }
+               
             }
-            else
-            {
-                st1.push(ch);
-            }
+             else
+                {
+                    a.push_back(ch);
+                }
         }
-         for(auto ch:t)
+         for(char ch:t)
         {
             if(ch=='#')
             {
-                if(!st2.empty())
-                st2.pop();
+                if(!b.empty())
+                {
+                    b.pop_back();
+                }
+                
             }
             else
-            {
-                st2.push(ch);
-            }
+                {
+                    b.push_back(ch);
+                }
         }
-        return isequal(st1,st2);
+        return a==b;
     }
 };
