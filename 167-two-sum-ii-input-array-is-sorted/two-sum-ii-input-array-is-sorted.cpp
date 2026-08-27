e997333fb06720cce2db97d4dc117b44456baac8
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-08-28 03:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.16%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,13 +5,13 @@ public:
    int i=0,j=n-1;
    while(i<j){
     if(numbers[i]+numbers[j]==target){
-    return{i+1,j+1};
+    break;
    }
     else if(numbers[i]+numbers[j]<target)
     i++;
     else
     j--;
    }
-   return {};
+   return {i+1,j+1};
     }
 };
