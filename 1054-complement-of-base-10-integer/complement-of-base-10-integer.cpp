a778f8a66882a0bb489960bd86e5eb9f1886efbd
vikash kumar <161820725+vikash9508@users.noreply.github.com>
2026-09-03 13:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.07%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,15 +3,25 @@ public:
     int bitwiseComplement(int n) {
         if(n==0)
         return 1;
-        int ans=0,rem,mul=1;
-        while(n)
-        {
-            rem=n%2;
-            rem^=1;
-            n/=2;
-            ans=ans+rem*mul;
-            mul*=2;
-        }
-        return ans;
+      string binary="";
+      while(n)
+      {
+        binary+=(n%2+'0');
+        n/=2;
+      } 
+      reverse(binary.begin(),binary.end());
+      for(char &c:binary)
+      {
+        if(c=='0')
+        c='1';
+        else
+        c='0';
+      }
+      int ans=0;
+      for(char c:binary)
+      {
+        ans=ans*2+(c-'0');
+      } 
+      return ans;
     }
 };
